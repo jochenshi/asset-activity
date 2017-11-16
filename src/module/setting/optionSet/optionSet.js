a3f7ee31877f50c738a85b84134eea461787f642
jochenshi <jochenshi@gmail.com>
2017-11-16 10:41:02 +0800

    add option set page and nav

---
+++ src/module/setting/optionSet/optionSet.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class OptionSet extends Component {
+    render () {
+        return (
+            <div>option set page</div>
+        )
+    }
+}
+
+export default OptionSet
