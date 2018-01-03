ffc48fe5e07e6169ed6af9de2db29ebc8180cd43
jochenshi <jochenshi@gmail.com>
2018-01-03 18:32:02 +0800

    supply equip start

--- src/module/device/equip/supplyEquip/addSupply/addSupplyForm.js
+++ src/module/device/equip/supplyEquip/addSupply/addSupplyForm.js
@@ -8,9 +8,26 @@ const FormItem = Form.Item;
 const Option = Select.Option;
 const {TextArea} = Input;
 
+import './addSupply.styl'
+
 class SupplyForm extends Component {
     constructor (props) {
-        super(props)
+        super(props);
+        this.state = {
+            type: [],
+            model: [],
+            brand: [],
+            origin: []
+        }
+    }
+    generateOption (type) {
+        let option = [];
+        if (this.state[type] && this.state[type].length) {
+            this.state[type].forEach((val) => {
+                option.push(<Option value={val.value} key={val.value}>{val.text}</Option>)
+            })
+        }
+        return option;
     }
     render () {
         const formItemLayout = {
@@ -21,6 +38,18 @@ class SupplyForm extends Component {
             wrapperCol: {
                 xs: {span: 24},
                 sm: {span: 16}
+            }
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0
+                },
+                sm: {
+                    span: 16,
+                    offset: 2
+                }
             }
         };
         const {getFieldDecorator} = this.props.form;
@@ -34,7 +63,7 @@ class SupplyForm extends Component {
                         ]
                     })(
                         <Select>
-                            {}
+                            {this.generateOption('origin')}
                         </Select>
                     )}
                 </FormItem>
@@ -43,7 +72,80 @@ class SupplyForm extends Component {
                         rules: [
                             {required: true, message: '请选择时间'}
                         ]
-                    })()}
+                    })(<DatePicker/>)}
+                </FormItem>
+                <FormItem {...formItemLayout} label={'源对象'}>
+                    {getFieldDecorator('originTarget')(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label={'目标对象'}>
+                    {getFieldDecorator('targetObject')(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label={'来源描述'}>
+                    {getFieldDecorator('ascDes')(
+                        <TextArea/>
+                    )}
+                </FormItem>
+                <h1 className={'form-field-title'}>配件信息</h1>
+                <FormItem {...formItemLayout} label={'名称'}>
+                    {getFieldDecorator('name',{
+                        rules: [
+                            {required: true, message: '名称不能为空'}
+                        ]
+                    })(<Input/>)}
+                </FormItem>
+                <FormItem {...formItemLayout} label='类型'>
+                    {getFieldDecorator('type',{
+                        rules: [
+                            {required: true, message: '类型不能为空'}
+                        ]
+                    })(
+                        <Select>
+                            {this.generateOption('type')}
+                        </Select>
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label='型号'>
+                    {getFieldDecorator('model',{
+                        rules: [
+                            {required: true, message: '型号不能为空'}
+                        ]
+                    })(
+                        <Select mode='combobox'>
+                            {this.generateOption('model')}
+                        </Select>
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label='品牌'>
+                    {getFieldDecorator('brand',{
+                        rules: [
+                            {required: true, message: '品牌不能为空'}
+                        ]
+                    })(
+                        <Select mode='combobox'>
+                            {this.generateOption('brand')}
+                        </Select>
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label='数量'>
+                    {getFieldDecorator('number',{
+                        rules: [
+                            {required: true, message: '数量不能为空'},
+                            {pattern: /^[1-9]\d*$/, message: '请输入正整数'}
+                        ]
+                    })(
+                        <Input style={{width: 200}}/>
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label='描述'>
+                    {getFieldDecorator('description')(<TextArea/>)}
+                </FormItem>
+                <FormItem {...tailFormItemLayout}>
+                    <Button type='primary' htmlType='submit'>确认</Button>
+                    <Button style={{marginLeft: 8}} onClick={() => {this.props.history.replace('/auth/main/deviceEquip/normalEquip')}}>取消</Button>
                 </FormItem>
             </Form>
         )
